d5112912ee06b14bc2368b7e85dd1fcaefafb673
Andrea De Pirro <andrea.depirro@softonic.com>
2019-08-07 09:39:06 +0200

    Fix tests

--- src/AccessTokenCacheHandler.php
+++ src/AccessTokenCacheHandler.php
@@ -58,6 +58,7 @@ class AccessTokenCacheHandler
 
     public function getCacheKey(OAuth2Provider $provider, array $options): string
     {
+        $query = [];
         parse_str(parse_url($provider->getAuthorizationUrl(), PHP_URL_QUERY), $query);
         return static::CACHE_KEY_PREFIX
             . md5($provider->getBaseAuthorizationUrl() . $query['client_id'] . serialize($options));
